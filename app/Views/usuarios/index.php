9a58f433469a73f9272efb914317c2c4545b46fd
jesusSan1 <jjsanru3@gmail.com>
2024-03-04 09:30:18 -0600

    refactor: formulario creado con el helper form

--- app/Views/usuarios/index.php
+++ app/Views/usuarios/index.php
@@ -10,7 +10,8 @@ Crear usuarios gerentes
                 <?=form_open('')?>
                 <div class="card-header pb-0">
                     <div class="d-flex align-items-center">
-                        <?=form_submit('guardar', 'Guardar', ['class' => 'btn btn-primary btn-sm ms-auto'])?>
+                        <?=anchor(base_url('dashboard'), 'Regresar', ['class' => 'btn btn-secondary btn-sm ms-auto'])?>
+                        <?=form_submit('guardar', 'Guardar', ['class' => 'btn btn-primary btn-sm ms-3'])?>
                     </div>
                 </div>
                 <div class="card-body">
@@ -18,38 +19,38 @@ Crear usuarios gerentes
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="example-text-input" class="form-control-label">Usuario</label>
-                                <input class="form-control" type="text" value="lucky.jesse">
+                                <?=form_label('Usuario', 'user', ['class' => 'form-control-label'])?>
+                                <?=form_input(['type' => 'text', 'name' => 'user', 'id' => 'user', 'class' => 'form-control', 'value' => old('user')])?>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="example-text-input" class="form-control-label">Correo electronico</label>
-                                <input class="form-control" type="email" value="jesse@example.com">
+                                <?=form_label('Correo electronico', 'email', ['class' => 'form-control-label'])?>
+                                <?=form_input(['type' => 'email', 'name' => 'email', 'id' => 'email', 'class' => 'form-control', 'value' => old('email')])?>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="example-text-input" class="form-control-label">Nombre</label>
-                                <input class="form-control" type="text" value="Jesse">
+                                <?=form_label('Nombre', 'name', ['class' => 'form-control-label'])?>
+                                <?=form_input(['type' => 'text', 'name' => 'name', 'id' => 'name', 'class' => 'form-control', 'value' => old('name')])?>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="example-text-input" class="form-control-label">Apellido paterno</label>
-                                <input class="form-control" type="text" value="Lucky">
+                                <?=form_label('Apellido paterno', 'apepat', ['class' => 'form-control-label'])?>
+                                <?=form_input(['type' => 'text', 'name' => 'apepat', 'id' => 'apepat', 'class' => 'form-control', 'value' => old('apepat')])?>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="example-text-input" class="form-control-label">Apellido materno</label>
-                                <input class="form-control" type="text" value="Lucky">
+                                <?=form_label('Apellido materno', 'apemat', ['class' => 'form-control-label'])?>
+                                <?=form_input(['type' => 'text', 'name' => 'apemat', 'id' => 'apemat', 'class' => 'form-control', 'value' => old('apemat')])?>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="example-text-input" class="form-control-label">Contraseña</label>
-                                <input class="form-control" type="text" value="Lucky">
+                                <?=form_label('Contraseña', 'password', ['class' => 'form-control-label'])?>
+                                <?=form_input(['type' => 'password', 'name' => 'password', 'id' => 'password', 'class' => 'form-control'])?>
                             </div>
                         </div>
                     </div>
@@ -58,15 +59,14 @@ Crear usuarios gerentes
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group">
-                                <label for="example-text-input" class="form-control-label">Dirección</label>
-                                <input class="form-control" type="text"
-                                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09">
+                                <?=form_label('Dirección', 'address', ['class' => 'form-control-label'])?>
+                                <?=form_input(['type' => 'text', 'name' => 'address', 'id' => 'address', 'class' => 'form-control', 'value' => old('address')])?>
                             </div>
                         </div>
                         <div class="col-md-4">
                             <div class="form-group">
-                                <label for="example-text-input" class="form-control-label">Telefono</label>
-                                <input class="form-control" type="tel" value="">
+                                <?=form_label('Telefono', 'phone', ['class' => 'form-control-label'])?>
+                                <?=form_input(['type' => 'tel', 'name' => 'phone', 'id' => 'phone', 'class' => 'form-control', 'value' => old('phone')])?>
                             </div>
                         </div>
                     </div>
