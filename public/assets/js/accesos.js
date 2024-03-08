9e58add738be9fe55762ec93f57de5a58ea3773e
jesusSan1 <jjsanru3@gmail.com>
2024-03-08 10:09:48 -0600

    refactor: refactorizar codigo para habilitar o deshabilitar usuarios

--- public/assets/js/accesos.js
+++ public/assets/js/accesos.js
@@ -1,61 +1,41 @@
+function data(element) {
+  const tr = element.parentNode.parentNode;
+  const id = tr.children[0].children[0].children[0].children[0].value;
+  const activo = tr.children[2].children[0];
+  const inactivo = tr.children[2].children[1];
+  return { tr, id, activo, inactivo };
+}
+
+async function sendData(element, formData, habilitado, param1, param2) {
+  const { id, activo, inactivo } = data(element);
+  formData.append("habilitado", habilitado);
+  formData.append("id", id);
+  fetch("habilitar", {
+    method: "POST",
+    body: formData,
+  }).then(() => {
+    activo.style.display = param1;
+    inactivo.style.display = param2;
+  });
+}
 document.querySelectorAll(".deshabilitar").forEach((element) => {
   element.addEventListener("change", (e) => {
-    const tr = element.parentNode.parentNode;
-    const id = tr.children[0].children[0].children[0].children[0].value;
-    const activo = tr.children[2].children[0];
-    const inactivo = tr.children[2].children[1];
     let formData = new FormData();
     if (e.target.checked) {
-      formData.append("habilitado", 1);
-      formData.append("id", id);
-      fetch("habilitar", {
-        method: "POST",
-        body: formData,
-      }).then(() => {
-        activo.style.display = "block";
-        inactivo.style.display = "none";
-      });
+      sendData(element, formData, 1, "block", "none");
     } else {
-      formData.append("habilitado", 0);
-      formData.append("id", id);
-      fetch("habilitar", {
-        method: "POST",
-        body: formData,
-      }).then(() => {
-        activo.style.display = "none";
-        inactivo.style.display = "block";
-      });
+      sendData(element, formData, 0, "none", "block");
     }
   });
 });
 
 document.querySelectorAll(".habilitar").forEach((element) => {
   element.addEventListener("change", (e) => {
-    const tr = element.parentNode.parentNode;
-    const id = tr.children[0].children[0].children[0].children[0].value;
-    const activo = tr.children[2].children[0];
-    const inactivo = tr.children[2].children[1];
     let formData = new FormData();
     if (e.target.checked) {
-      formData.append("id", id);
-      formData.append("habilitado", 1);
-      fetch("habilitar", {
-        method: "POST",
-        body: formData,
-      }).then(() => {
-        inactivo.style.display = "none";
-        activo.style.display = "block";
-      });
+      sendData(element, formData, 1, "block", "none");
     } else {
-      formData.append("id", id);
-      formData.append("habilitado", 0);
-      fetch("habilitar", {
-        method: "POST",
-        body: formData,
-      }).then(() => {
-        inactivo.style.display = "block";
-        activo.style.display = "none";
-      });
+      sendData(element, formData, 0, "none", "block");
     }
   });
 });
