f5d78a4ca06b221ed387c29b8a6769daf35c41dc
jesusSan1 <jjsanru3@gmail.com>
2024-03-13 15:40:12 -0600

    feacture: guardar los datos de tipo de vehiculos

--- app/Controllers/TipoVehiculos.php
+++ app/Controllers/TipoVehiculos.php
@@ -3,12 +3,44 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\TipoVehiculosModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class TipoVehiculos extends BaseController
 {
+    protected $tipoVehiculos;
+    protected $security;
+
+    public function __construct()
+    {
+        $this->tipoVehiculos = model(TipoVehiculosModel::class);
+        $this->security = \Config\Services::security();
+    }
     public function index()
     {
+        if ($this->request->is('post')) {
+            $rules = [
+                'type' => [
+                    'label' => 'tipo de vehiculo',
+                    'rules' => 'required|alpha|min_length[3]',
+                    'errors' => [
+                        'required' => 'El {field} debe ser llenado',
+                        'alpha' => 'El {field} no debe tener caracteres especiales o numeros',
+                        'min_length' => 'El {field} debe tener al menos {param} caracteres',
+                    ],
+                ],
+            ];
+            if (!$this->validate($rules)) {
+                return redirect()->back()->with('list', $this->validation->getErrors())->withInput();
+            }
+            $data = [
+                'nombre' => $this->security->sanitizeFilename(trim($this->request->getPost('type'))),
+                'fecha_registro' => date('Y-m-d'),
+                'id_usuario' => session('id'),
+            ];
+            $this->tipoVehiculos->insert($data);
+            return redirect()->back()->with('success', 'Datos guardados correctamente');
+        }
         return view('tipoVehiculos\index');
     }
 }
--- app/Views/tipoVehiculos/index.php
+++ app/Views/tipoVehiculos/index.php
@@ -1,12 +1,13 @@
-<?= $this->extend('layouts\dashboard'); ?>
-<?= $this->section('titulo'); ?>
+<?=$this->extend('layouts\dashboard');?>
+<?=$this->section('titulo');?>
 Tipos de vehiculos
-<?= $this->endSection(); ?>
-<?= $this->section('contenido'); ?>
+<?=$this->endSection();?>
+<?=$this->section('contenido');?>
 <div class="container-fluid py-4">
     <div class="row">
         <div class="col-md-12">
             <div class="card">
+                <?=form_open('')?>
                 <div class="card-header pb-0">
                     <div class="d-flex align-items-center">
                         <?=anchor(base_url('dashboard'), 'Regresar', ['class' => 'btn btn-secondary btn-sm ms-auto'])?>
@@ -28,50 +29,48 @@ Tipos de vehiculos
                             </div>
                         </div>
                     </div>
+                    <?=form_close()?>
                     <div class="card">
-                        <div class="card">
-                            <div class="table-responsive rounded">
-                                <table class="table align-items-center mb-0">
-                                    <thead>
-                                        <tr>
-                                            <th
-                                                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                                Author</th>
-                                            <th
-                                                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
-                                                Function</th>
-                                            <th
-                                                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                                Employed</th>
-                                            <th class="text-secondary opacity-7"></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <div class="d-flex px-2 py-1">
+                        <div class="table-responsive rounded">
+                            <table class="table align-items-center mb-0">
+                                <thead class="table-dark">
+                                    <tr>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Author</th>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                            Function</th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Employed</th>
+                                        <th class="text-secondary opacity-7"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <div class="d-flex px-2 py-1">
 
-                                                    <div class="d-flex flex-column justify-content-center">
-                                                        <p class="text-xs text-secondary mb-0">john@creative-tim.com</p>
-                                                    </div>
+                                                <div class="d-flex flex-column justify-content-center">
+                                                    <p class="text-xs text-secondary mb-0">john@creative-tim.com</p>
                                                 </div>
-                                            </td>
-                                            <td>
-                                                <p class="text-xs text-secondary mb-0">Organization</p>
-                                            </td>
-                                            <td class="align-middle text-center">
-                                                <span class="text-secondary text-xs font-weight-bold">23/04/18</span>
-                                            </td>
-                                            <td class="align-middle">
-                                                <a href="javascript:;" class="text-secondary font-weight-bold text-xs"
-                                                    data-toggle="tooltip" data-original-title="Edit user">
-                                                    Edit
-                                                </a>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs text-secondary mb-0">Organization</p>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="text-secondary text-xs font-weight-bold">23/04/18</span>
+                                        </td>
+                                        <td class="align-middle">
+                                            <a href="javascript:;" class="text-secondary font-weight-bold text-xs"
+                                                data-toggle="tooltip" data-original-title="Edit user">
+                                                Edit
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -80,4 +79,4 @@ Tipos de vehiculos
 
     </div>
 </div>
-<?= $this->endSection(); ?>
+<?=$this->endSection();?>
